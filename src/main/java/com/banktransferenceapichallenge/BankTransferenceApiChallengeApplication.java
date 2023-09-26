e8d6cc702e5ffb5ac4a7ad197f09e4ba60fc86ab
Jhonatan Henkel <jhowrf@gmail.com>
2023-09-26 20:46:04 -0300

    Iniciando projeto com o spring

---
+++ src/main/java/com/banktransferenceapichallenge/BankTransferenceApiChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.banktransferenceapichallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankTransferenceApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankTransferenceApiChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/banktransferenceapichallenge/BankTransferenceApiChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banktransferenceapichallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankTransferenceApiChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
