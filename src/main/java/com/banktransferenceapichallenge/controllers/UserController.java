305ef10f2bd53d908cbd94a9831dbfa782610de6
Jhonatan Henkel <jhowrf@gmail.com>
2023-09-27 07:12:54 -0300

    Corrigindo alguns erros na api de usuários

--- src/main/java/com/banktransferenceapichallenge/controllers/UserController.java
+++ src/main/java/com/banktransferenceapichallenge/controllers/UserController.java
@@ -7,19 +7,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.banktransferenceapichallenge.domain.user.User;
 import com.banktransferenceapichallenge.dtos.UserDTO;
 import com.banktransferenceapichallenge.services.UserService;
 
-@RestController("/users")
+@RestController()
+@RequestMapping("/users")
 public class UserController {
     @Autowired
     private UserService userService;
 
     @PostMapping
-    public ResponseEntity<User> createUser(UserDTO user) {
+    public ResponseEntity<User> createUser(@RequestBody UserDTO user) {
         User newUser = this.userService.createUser(user);
         return new ResponseEntity<>(newUser, HttpStatus.CREATED);
     }
